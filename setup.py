c6827156b9479f798dac0774a919b7614325070a
Panagiotis Matigakis <pmatigakis@gmail.com>
2020-04-11 13:12:21 +0300

    Bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md") as f:
 
 setup(
     name="metricslib",
-    version="0.1.0",
+    version="0.2.0",
     description="Metrics collection library",
     ling_description=long_description,
     long_description_content_type="text/markdown",
